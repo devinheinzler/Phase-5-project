95fe8640663d28ca0e20701c837cce1def22fb71
devinheinzler <devinheinzler271@gmail.com>
2022-12-19 14:28:02 -0600

    added serializers and route resources

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,3 +1,3 @@
 class CampaignSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :campaign_title, :campaign_story, :campaign_setting, :campaign_NPCs, :campaign_challenges, :campaign_aftermath
 end
--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,3 +1,3 @@
 class CharacterSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :user_id, :campaign_id :character_name, :character_image, :race_name, :race_image, :race_description, :race_trait_name, :subrace_name, :class_name, :class_image, :subclass, :subclass_feature, :race_proficiencies, :character_background, :character_language, :character_alignment, :character_notes
 end
