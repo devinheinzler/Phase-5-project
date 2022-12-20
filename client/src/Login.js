09ae090d00df0f32298fed23f1b64ddfe1a933b0
devinheinzler <devinheinzler271@gmail.com>
2022-12-20 16:53:06 -0600

    Added react-router-dom, added components, login

---
+++ client/src/Login.js
@@ -0,0 +1,69 @@
+import {useState} from 'react'
+
+function Login({toggleLoggedIn}) {
+    const [formData, setFormData] = useState({email: "", password: ""})
+
+    function onLogin() {
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(res => res.json())
+        .then(r => {
+            if (r.email) {
+                localStorage.removeItem("error")
+                localStorage.email = r.email
+            } else {
+                localStorage.error = "invalid email/password combination"
+            }
+            setFormData({email: "", password: ""})
+            toggleLoggedIn()
+        })
+    }
+
+    function logout() {
+        fetch("http://localhost:3000/logout")
+        .then(setFormData({email: "", password: ""}))
+        localStorage.removeItem("email")
+        toggleLoggedIn()
+    }
+
+    return(
+        localStorage.email ?
+        
+        <div className='logout-align'>
+        <p>
+            <Message  compact>
+                logged in as {localStorage.email}!
+            </Message>
+        <p>
+            <Button icon labelPosition='right' onClick={() => logout()}>
+                Logout
+                <Icon name='right arrow' />
+            </Button>
+        </p>
+        </p>
+        </div>
+        :
+        <div className='login-align'>
+            <p>
+                <b><small>{localStorage.error ? localStorage.error : null}</small></b><br />
+                <Input icon='users' iconPosition='left' placeholder='Input Username...' type='text' name='email' id='email' value={formData["email"]} onChange={(e) => setFormData({...formData, email: e.target.value})} />
+            </p>
+            <p>
+                <Input icon='lock' iconPosition='left' placeholder='Input Password...' type="password" name="password" id="password" value={formData["password"]} onChange={(e) => setFormData({...formData, password: e.target.value})} />
+
+            </p>
+            <Button icon labelPosition='right' onClick={() => onLogin()}>
+                Login
+                <Icon name='right arrow' />
+            </Button>
+        </div>
+    )
+}
+
+export default Login
